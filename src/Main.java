4362c393abd0fe36b7d82cff3a7014744ae435b2
Yralec <hipigmaster@gmail.com>
2016-07-17 22:30:55 +0200

    less

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//I'll probably comment something
-		//This too
-		//another one
+		//asdf
 	}
 
 }
