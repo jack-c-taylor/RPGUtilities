16f549eed9141e9c749250aa186d80673a2417d1
Zaleos <jackctaylor97@gmail.com>
2016-07-22 22:15:43 +0100

    Dice changes
    Added dice mechanics local to the dice tab (still need to get it to
    affect the dice pane)

---
+++ src/Dice.java
@@ -0,0 +1,16 @@
+import javax.swing.JButton;
+
+public class Dice extends JButton {
+	
+	private static final long serialVersionUID = 1L;
+	private int maxRand;
+	
+	public Dice(int maxRand){
+		this.maxRand=maxRand;
+		setText(maxRand+"");
+	}
+	
+	public int getMaxRand(){
+		return maxRand;
+	}
+}
--- src/TabDice.java
+++ src/TabDice.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TabDice extends JPanel {
@@ -5,6 +9,15 @@ public class TabDice extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	public TabDice(){
-		
+		Random r=new Random();
+		JLabel output=new JLabel();
+		int[] numbers={4,6,8,10,12,20,100};
+		ArrayList<Dice> pl = new ArrayList<Dice>();
+		for (int i=0;i<numbers.length;i++){
+			pl.add(new Dice(numbers[i]));
+			pl.forEach(p-> p.addActionListener(e -> output.setText(r.nextInt(p.getMaxRand())+"")));
+		}
+		pl.forEach(p-> add(p));
+		add(output);
 	}
 }
