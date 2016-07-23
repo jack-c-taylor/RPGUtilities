fc82660d3671a3a30817f9c821f20b5c31f95642
Zaleos <jackctaylor97@gmail.com>
2016-07-23 20:55:21 +0100

    Dice Section Update
    Improved the layout and added custom dice rolls.

--- src/TabDice.java
+++ src/TabDice.java
@@ -1,23 +1,63 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class TabDice extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private ArrayList<Dice> diceList;
+	private JLabel output;
+	private JTextArea customPane;
+	private JButton customButton;
+	private Random r;
 
 	public TabDice(){
-		Random r=new Random();
-		JLabel output=new JLabel();
+		output=new JLabel("You rolled a...");
+		customPane=new JTextArea();
+		customButton=new JButton("Roll");
+		r=new Random();
+		
 		int[] numbers={4,6,8,10,12,20,100};
-		ArrayList<Dice> pl = new ArrayList<Dice>();
+		diceList = new ArrayList<Dice>();
 		for (int i=0;i<numbers.length;i++){
-			pl.add(new Dice(numbers[i]));
-			pl.forEach(p-> p.addActionListener(e -> output.setText(r.nextInt(p.getMaxRand())+"")));
+			diceList.add(new Dice(numbers[i]));
 		}
-		pl.forEach(p-> add(p));
-		add(output);
+		diceList.forEach(p-> p.addActionListener(e -> output.setText("You rolled a "+(r.nextInt(p.getMaxRand())+1)+".")));
+		customButton.addActionListener(e -> output.setText(rollCustomNumber()));
+		createGridBag();
+	}
+	
+	public void createGridBag(){
+		setLayout(new GridBagLayout());
+		add(output,createGridBagConstraints(0,0));
+		for (int i=0;i<diceList.size();i++){
+			add(diceList.get(i),createGridBagConstraints(i,1));
+		}
+		add(new JLabel("Custom Dice Size: "),createGridBagConstraints(0,2));
+		add(customPane,createGridBagConstraints(1,2));
+		add(customButton,createGridBagConstraints(2,2));
+	}
+	
+	public GridBagConstraints createGridBagConstraints(int x, int y){
+		GridBagConstraints c = new GridBagConstraints();
+		c.weightx=1;
+		c.gridx=x;
+		c.gridy=y;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		return c;
+	}
+	
+	public String rollCustomNumber(){
+		try{
+			int rand=r.nextInt(Integer.parseInt(customPane.getText()))+1;
+			return "You rolled a "+rand+".";
+		}catch (Exception e){}
+		return "Invalid input.";
 	}
 }
